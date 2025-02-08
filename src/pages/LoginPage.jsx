33f9b6e81c8608d49f34ce366630ef12835ac9f4
Meshach Vijaya <meshachvijaya@gmail.com>
2025-02-08 12:24:10 +0700

    first commit

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,24 @@
+const LoginPage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="login">
+                <h2 className="text-center">Login</h2>
+                <form>
+                    <div className="each-input">
+                        <label className="mr-4">Username</label>
+                        <br/>
+                        <input type="text" className="input"/>
+                    </div>
+                    <div className="each-input">
+                        <label className="mr-4">Password</label>
+                        <br/>
+                        <input type="password" className="input"/>
+                    </div>
+                    <button type="submit" className="button">Login</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
